c81f4d1f670549820b856aba1ff2a0fe139d408e
yjhyjhyjh0 <yjhyjhyjh0@gmail.com>
2020-10-14 10:02:18 +0800

    Update user name

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+//import org.apache.logging.log4j.LogManager;
+//import org.apache.logging.log4j.Logger;
+//
+public class Main {
+//    private static Logger log = LogManager.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.testLogging();
+        System.out.println("hello");
+    }
+
+    private void testLogging() {
+//        log.info("hello info");
+//        log.debug("hello debug");
+    }
+}
